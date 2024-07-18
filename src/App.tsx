a0b5a6aec05b9c4b7a6d16de620403682ee9db04
yekang <wb-yekang@rokid.com>
2024-07-18 15:47:28 +0800

    fix: 完成通信

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import type { EventCenterForMicroApp } from '@micro-zoe/micro-app'
 import '@/app.less'
 
 function App() {
-  return <div>我是 React 子应用</div>
+  const [state, setState] = useState<{ content: string }>({ content: '' })
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const ma: EventCenterForMicroApp = (window as any).microApp
+    const listener = (data: { content: string }) => {
+      console.log('来自主应用的数据', data)
+      setState(data)
+    }
+    const globalListener = (data: { content: string }) => {
+      console.log('==React子应用接收全局数据', data)
+      setState(data)
+    }
+    ma.addDataListener(listener, true)
+    ma.addGlobalDataListener(globalListener)
+  }, [])
+  const onSendParent = () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const ma: EventCenterForMicroApp = (window as any).microApp
+    ma.forceDispatch({
+      content: '我是React子应用发送的消息！！！！'
+    })
+  }
+  return (
+    <div>
+      我是 React 子应用 <button onClick={onSendParent}>向父应用发送消息</button>
+      {state.content && (
+        <div style={{ color: 'red' }}>
+          <div>{state.content}</div>
+        </div>
+      )}
+    </div>
+  )
 }
 export default App
